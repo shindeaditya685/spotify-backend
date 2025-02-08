c881cbb01e7f4c45329d5e56472b7d805352f402
Aditya Shinde <shindeaditya685@gmail.com>
2025-02-08 09:33:01 +0530

    Song route and controller

--- src/routes/songs.route.js
+++ src/routes/songs.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
+import {
+  getAllSongs,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/song.controller.js";
 
 const router = Router();
+
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
